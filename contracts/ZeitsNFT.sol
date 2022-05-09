7688f8a198bdc38186b135014d8030e91511ad33
Antons Tesluks <anton.tesliuk@gmail.com>
2022-05-10 00:17:55 +0400

    Created Zeits NFT contract and tests

---
+++ contracts/ZeitsNFT.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+// ERC721A Contracts v3.3.0
+// Creator: Chiru Labs
+
+pragma solidity ^0.8.4;
+
+import './ERC721A.sol';
+
+/**
+ * @dev Implementation of https://eips.ethereum.org/EIPS/eip-721[ERC721] Non-Fungible Token Standard, including
+ * the Metadata extension. Built to optimize for lower gas during batch mints.
+ *
+ * Assumes serials are sequentially minted starting at _startTokenId() (defaults to 0, e.g. 0, 1, 2, 3..).
+ *
+ * Assumes that an owner cannot have more than 2**64 - 1 (max value of uint64) of supply.
+ *
+ * Assumes that the maximum token id cannot exceed 2**256 - 1 (max value of uint256).
+ */
+contract ZeitsNFT is ERC721A {
+    using Strings for uint256;
+
+    // Base uri
+    string private _baseUri;
+
+    constructor(string memory name_, string memory symbol_, string memory baseUri_) ERC721A(name_, symbol_) {
+        require(bytes(baseUri_).length > 0, 'Base uri is empty');
+        _baseUri = baseUri_;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length != 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), '.json')) : '';
+    }
+
+    function numberMinted(address owner) public view returns (uint256) {
+        return _numberMinted(owner);
+    }
+
+    function totalMinted() public view returns (uint256) {
+        return _totalMinted();
+    }
+
+    function getAux(address owner) public view returns (uint64) {
+        return _getAux(owner);
+    }
+
+    function setAux(address owner, uint64 aux) public {
+        _setAux(owner, aux);
+    }
+
+    function baseURI() public view returns (string memory) {
+        return _baseURI();
+    }
+
+    function exists(uint256 tokenId) public view returns (bool) {
+        return _exists(tokenId);
+    }
+
+    function safeMint(address to, uint256 quantity) public {
+        _safeMint(to, quantity);
+    }
+
+    function safeMint(
+        address to,
+        uint256 quantity,
+        bytes memory _data
+    ) public {
+        _safeMint(to, quantity, _data);
+    }
+
+    function mint(address to, uint256 quantity) public {
+        _mint(to, quantity);
+    }
+
+    function burn(uint256 tokenId, bool approvalCheck) public {
+        _burn(tokenId, approvalCheck);
+    }
+
+    /**
+     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
+     * by default, can be overriden in child contracts.
+     */
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseUri;
+    }
+}
